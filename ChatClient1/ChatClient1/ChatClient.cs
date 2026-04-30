f315d370a0a1d8bff1bca293ce1599d2a7e1ea4e
FaiSonn <faisonno@gmail.com>
2026-04-30 12:40:09 +0300

    add base ChatClient class with fields and events

--- ChatClient1/ChatClient1/ChatClient.cs
+++ ChatClient1/ChatClient1/ChatClient.cs
@@ -1,1 +1,21 @@
-﻿
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace ChatClient1
+{
+    public class ChatClient
+    {
+        private TcpClient _tcpClient;
+        private StreamReader _reader;
+        private StreamWriter _writer;
+        private Thread _readThread;
+        private bool _isConnected;
+        private string _nickname;
+
+        public event Action OnConnected;
+        public event Action OnDisconnected;
+        public event Action<string> OnMessageReceived;
+        public event Action<string> OnError;
