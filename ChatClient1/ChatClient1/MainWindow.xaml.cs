b6d62af76af4827d3fe26dd200d5142227763d22
FaiSonn <faisonno@gmail.com>
2026-04-30 12:47:03 +0300

    initialize MainWindow and integrate ChatClient

--- ChatClient1/ChatClient1/MainWindow.xaml.cs
+++ ChatClient1/ChatClient1/MainWindow.xaml.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Media;
+
+namespace ChatClient1
+{
+    public partial class MainWindow : Window
+    {
+        private readonly ChatClient _chatClient;
+        private readonly ObservableCollection<ChatMessage> _messages = new ObservableCollection<ChatMessage>();
+        private string _myNickname;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            MessagesList.ItemsSource = _messages;
+            _chatClient = new ChatClient();
+            SubscribeEvents();
+        }
